66f8602caac7097a633322c5024334003b3b02d3
Paula <psanzilundain@gmail.com>
2024-07-10 12:40:40 +0200

    exercise año bisiesto

---
+++ javascript/2.2/switch/3/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const year = 4;
+
+// año bisiesto cada 4 años a partir del año 0
+
+switch (year) {
+  case 3:
+    console.log('El siguiente año bisiesto será el 4');
+    break;
+  case 4:
+    console.log('El siguiente año bisiesto será el 8');
+    break;
+  default:
+    console.log('Idk');
+}
