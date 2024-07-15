fbc974e1c3497bc3ee169fe99d751e91823ac442
Paula <psanzilundain@gmail.com>
2024-07-15 19:21:42 +0200

    bio de adalabers

---
+++ javascript/2.7/1/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const adalaber1 = {
+  name: 'Susana',
+  age: 34,
+  job: 'periodista',
+};
+
+const adalaber2 = {
+  name: 'Rocío',
+  age: 25,
+  job: 'actriz',
+};
+
+console.log(
+  'Mi nombre es ' +
+    adalaber1.name +
+    ', tengo ' +
+    adalaber1.age +
+    ' años y soy ' +
+    adalaber1.job +
+    '.'
+);
+
+console.log(
+  'Mi nombre es ' +
+    adalaber2.name +
+    ', tengo ' +
+    adalaber2.age +
+    ' años y soy ' +
+    adalaber2.job +
+    '.'
+);
