8c729dc2187d23311c0211faf2d482591657b3a8
Paula <psanzilundain@gmail.com>
2024-07-17 21:29:42 +0200

    arbol de navidad

--- javascript/2.11/3/main.js
+++ javascript/2.11/3/main.js
@@ -1,1 +1,18 @@
 'use strict';
+const list = document.querySelector('ul');
+let tree = [];
+
+const makeTree = (numberOfLines) => {
+  tree.push('★');
+  for (let i = 0; i < numberOfLines + 1; i++) {
+    let line = '';
+    for (let j = 0; j < i; j++) {
+      line += '▲';
+    }
+    tree.push(line);
+  }
+  for (let line of tree) {
+    console.log(line);
+  }
+};
+tree = makeTree(5);
