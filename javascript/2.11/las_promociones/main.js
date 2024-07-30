1a19fd20aba56fab1fa2658f7a0d9268768db830
Paula <psanzilundain@gmail.com>
2024-07-30 13:29:52 +0200

    more exercises on loops

--- javascript/2.11/las_promociones/main.js
+++ javascript/2.11/las_promociones/main.js
@@ -116,11 +116,58 @@ const studentsWorkingInGoogle = ['id-2', 'id-3', 'id-5', 'id-9'];
 
 //5 - Pintar en pantalla los nombres de las promos y los nombres, la edad y el id de las alumnas
 
-console.log(promos[0].students);
+// for (let i = 0; i < promos.length; i++) {
+//   list.innerHTML += `<ul><li><p>Nombre: ${promos[i].name}</p></li></ul>`;
+//   for (let j = 0; j < promos[i].students.length; j++) {
+//     list.innerHTML += `<ul><li ${promos[i].students[j].id}>${promos[i].students[j].name}, ${promos[i].students[j].age}</li></ul>`;
+//   }
+// }
 
-for (let i = 0; i < promos.length; i++) {
-  list.innerHTML += `<ul><li><p>Nombre: ${promos[i].name}</p></li></ul>`;
-  for (let j = 0; j < promos[i].students.length; j++) {
-    list.innerHTML += `<ul><li ${promos[i].students[j].id}>${promos[i].students[j].name}, ${promos[i].students[j].age}</li></ul>`;
-  }
-}
+// for (let promo of promos) {
+//   list.innerHTML += `<ul><li><p>Nombre: ${promo.name}</p></li></ul>`;
+//   for (let student of promo.students) {
+//     list.innerHTML += `<ul><li ${student.id}>${student.name}, ${student.age}</li></ul>`;
+//   }
+// }
+
+//6 - Pintar en pantalla los nombres de las promos y los nombres, la edad y el id de las alumnas que tenga edad par
+
+// for (let i = 0; i < promos.length; i++) {
+//   list.innerHTML += `<ul><li><p>Nombre: ${promos[i].name}</p></li></ul>`;
+//   for (let j = 0; j < promos[i].students.length; j++) {
+//     if (promos[i].students[j].age % 2 === 0) {
+//       list.innerHTML += `<ul><li ${promos[i].students[j].id}>${promos[i].students[j].name}, ${promos[i].students[j].age}</li></ul>`;
+//     }
+//   }
+// }
+
+// for (let promo of promos) {
+//   list.innerHTML += `<ul><li><p>Nombre: ${promo.name}</p></li></ul>`;
+//   for (let student of promo.students) {
+//     if (student.age % 2 === 0) {
+//       list.innerHTML += `<ul><li ${student.id}>${student.name}, ${student.age}</li></ul>`;
+//     }
+//   }
+// }
+
+//7- Pintar en pantalla los nombres de las promos y los nombres de la edad y el id de las alumnas que trabajan en Google
+
+// for (let i = 0; i < promos.length; i++) {
+//   list.innerHTML += `<ul><li><p>Nombre: ${promos[i].name}</p></li></ul>`;
+//   for (let j = 0; j < promos[i].students.length; j++) {
+//     if (studentsWorkingInGoogle.includes(promos[i].students[j].id)) {
+//       list.innerHTML += `<ul><li ${promos[i].students[j].id}>${promos[i].students[j].name}, ${promos[i].students[j].age}</li></ul>`;
+//     }
+//   }
+// }
+
+// for (let promo of promos) {
+//   list.innerHTML += `<ul><li><p>Nombre: ${promo.name}</p></li></ul>`;
+//   for (let student of promo.students) {
+//     if (studentsWorkingInGoogle.includes(student.id)) {
+//       list.innerHTML += `<ul><li>${student.name}, ${student.age}</li></ul>`;
+//     }
+//   }
+// }
+
+//8 - Pintar en pantalla los nombres de las promos y los nombres, la edad y el id de las alumnas y pintar el id de la alumna en consola cuando se haga click en una de ellas
